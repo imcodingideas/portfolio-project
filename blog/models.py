3247ac9ccb137f4fe78581270dfabb83a1895d86
Joseph Chambers <joseph@michael-chambers.com>
2018-03-25 19:31:02 -0700

    blog post, summary, different date format.

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ class Blog(models.Model):
   title = models.CharField(max_length=75)
   pub_date = models.DateTimeField()
   body = models.TextField()
+
+  def __str__(self):
+    return self.title
+
+  def summary(self):
+    return self.body[:100]
+
+  def pub_date_pretty(self):
+    return self.pub_date.strftime('%b %e %Y')
